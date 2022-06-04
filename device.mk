3810277c2faf78ceaa3c5dc2b7680c6f9ada99aa
EndCredits <alicization.han@gmail.com>
2022-06-04 11:26:02 +0800

    picasso: Build fastboot HAL into recovery

--- device.mk
+++ device.mk
@@ -19,6 +19,7 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 # fastbootd
 PRODUCT_PACKAGES += \
     android.hardware.fastboot@1.0-impl-mock \
+    android.hardware.fastboot@1.0-impl-mock.recovery \
     fastbootd
 
 # qcom decryption
