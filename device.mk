2619a6f7e69497acfff0f4f3c907141f04546ca8
sekaiacg <sekaiacg@gmail.com>
2022-07-22 09:12:19 +0800

    device: Configure SDCard replacement functionality
    Signed-off-by: sekaiacg <sekaiacg@gmail.com>

--- device.mk
+++ device.mk
@@ -17,6 +17,9 @@
 # Configure Virtual A/B
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 
+# Configure SDCard replacement functionality
+$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
+
 PRODUCT_PACKAGES += \
     bootctrl.xiaomi_sm8250.recovery \
     android.hardware.boot@1.1-impl-qti.recovery \
