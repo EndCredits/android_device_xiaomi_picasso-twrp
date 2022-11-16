de1aaeef4f520734b1aea2937eb93dde7359487e
EndCredits <alicization.han@gmail.com>
2022-11-17 01:41:40 +0800

    picasso: Move FBEv2 props to standalone file

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -59,14 +59,6 @@ BOARD_ROOT_EXTRA_FOLDERS := metadata
 TW_USE_FSCRYPT_POLICY := 2
 OF_FIX_DECRYPTION_ON_DATA_MEDIA := 1
 
-# Properties
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.crypto.allow_encrypt_override=true \
-	ro.crypto.dm_default_key.options_format.version=2 \
-	ro.crypto.volume.filenames_mode=aes-256-cts \
-	ro.crypto.volume.metadata.method=dm-default-key \
-	ro.crypto.volume.options=::v2
-
 # DTB
 BOARD_BOOT_HEADER_VERSION := 2
 
